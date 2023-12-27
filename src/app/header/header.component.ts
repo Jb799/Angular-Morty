a520d1c8fcc1ab9c5b271fe74db521112a2da25f
Your Name <jean-baptiste.relave@isen.yncrea.fr>
2023-12-27 17:41:35 +0100

    first commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+
+export class HeaderComponent implements OnInit {
+    pages: Array<any> = new Array<any>()
+
+    ngOnInit(): void {
+      this.pages.push( { name: 'Home', route: '/home' } )
+      this.pages.push( { name: 'Characters', route: '/character' } )
+      this.pages.push( { name: 'About', route: '/about' } )
+    }
+}
